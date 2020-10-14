61c11ad8861b2363a4eab0360962560a55340bd4
Carly de Frondeville <carlydefrondeville@MacBook-Pro-2.localdomain>
2020-10-14 03:15:44 -0700

    deal not appending to p.Hand properly

--- game.go
+++ game.go
@@ -8,32 +8,44 @@ import (
 // i read that global vars are problematic, but idk why
 // could replace this with a struct, but that seemed unnecessary
 var players []Player // dealing order matters
-var dck deck.Deck
+var dk deck.Deck
 var dealToNext int
 
-func startGame() {
+func StartGame() {
+	fmt.Println("Starting game...")
 	names := []string{"me", "dealer"} // dealer must be last
 	for _, n := range names {
 		players = append(players, Player{Name: n})
 	}
-	dck = deck.New(deck.WithShuffle())
+	dk = deck.New(deck.WithShuffle())
 	dealToNext = 0
 	deal(2)
 }
 
 func deal(numCards int) {
-	fmt.Printf("Dealing %v cards to all players", numCards)
+	fmt.Printf("Dealing %v cards to all players\n\n", numCards)
 	for i := 0; i < numCards; i++ {
 		for j, _ := range players {
 			p := players[(dealToNext + j) % len(players)]
-			p.Hand = append(p.Hand, draw())
+			fmt.Printf("appending to player %v(%v)\n", p.Name, j)
+			p.Hand = append(p.Hand, draw()) // this is not working
 		}
 	}
 	dealToNext = (dealToNext + 1) % len(players)
 }
 
 func draw() deck.Card {
-	c := dck[0]
-	dck = dck[1:]
+	c := dk[0]
+	dk = dk[1:]
 	return c
+}
+
+func PrintStatus() {
+	fmt.Println("### GAME STATUS ###")
+	fmt.Printf("Cards in deck: %v\n", len(dk))
+	fmt.Printf("Deal to next: %v(%v)\n", players[dealToNext].Name, dealToNext)
+	fmt.Println("List of players:")
+	for _, p := range players {
+		fmt.Print(p)
+	}
 }
--- player.go
+++ player.go
@@ -1,10 +1,21 @@
 package blackjack
 
 import (
+	"fmt"
 	"github.com/gophercises/deck"
 )
 
 type Player struct {
 	Name string
 	Hand []deck.Card
+}
+
+func (p Player) String() string {
+	s := fmt.Sprintf("%v: %v", p.Name, len(p.Hand))
+	for _, c := range p.Hand {
+		s += fmt.Sprintf("poop")
+		s += fmt.Sprintf("%v, ", c)
+	}
+	s += "\n"
+	return s
 }
